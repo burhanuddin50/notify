92be5cba35ab9651ff669d4b8f0247ec70881da1
Burhanuddin <burhanuddintinwala43@gmail.com>
2022-09-07 14:02:09 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+  let textarea = document.getElementById("exampleFormControlTextarea1");
+  let headarea = document.getElementById("exampleFormControlTextarea2");
+  function display(){
+    document.getElementById("add").innerHTML="";
+    let a=JSON.parse(localStorage.getItem("notes"));
+    a.forEach(element => {
+      if(element!=null){
+      document.getElementById("add").innerHTML += `<div class="w-auto p-8 border-solid border-black border-2 m-5 rounded-lg"><h1 class="text-2xl font-bold ">
+      ${element[0]}
+    </h1>
+    <p> ${element[1]}</p>
+    <button class="border-cyan-300 border-2 p-2 border-solid mx-11 w-auto" id="${element[0]}">Delete Note</button></div>`;
+  }});
+  del();}
+    function del(){
+      let b=JSON.parse(localStorage.getItem("notes"));
+      if(b!=null){
+      b.forEach(element=>{
+            if(element!=null){
+            document.getElementById(element[0]).addEventListener("click",()=>{
+              delete b[b.indexOf(element)];
+              localStorage.setItem("notes",JSON.stringify(b));
+              display();
+            })
+      }});
+    }}
+    if(localStorage.getItem("notes")!=null)
+    {
+    display();}
+    del();
+    
+  document.getElementById("bt").addEventListener("click", () => {
+      let notes=localStorage.getItem("notes");
+      if(notes == null)
+      {
+        notesObj=[];
+      }
+      else
+      {
+        notesObj=JSON.parse(notes);
+      }
+      notesObj.push([headarea.value,textarea.value]);
+      localStorage.setItem("notes",JSON.stringify(notesObj));
+      headarea.value = '';
+      textarea.value = '';
+      display();
+      del();
+  });
